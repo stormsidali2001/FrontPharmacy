07b410d21918a43a628f020fbbb294994f121520
shAtud <sidalihouda.computerscience@gmail.com>
2022-06-11 22:39:52 +0100

    setting the redux store and

---
+++ store/models/cart.model.ts
@@ -0,0 +1,15 @@
+export interface CartState{
+
+}
+export interface CartActions{
+
+}
+export interface CartThunks{
+
+}
+
+export interface CartModel extends CartState,CartActions,CartThunks{}
+
+export const cartModel:CartModel = {
+
+}
---
+++ store/models/index.ts
@@ -0,0 +1,12 @@
+import { CartModel,cartModel } from "./cart.model"
+
+export interface Model{
+    cartModel:CartModel;
+
+}
+
+
+export const model:Model = {
+    cartModel,
+
+}
