66f051cad308d2a3edf503ac3be2ac4ab3552fb2
shAtud <sidalihouda.computerScience@gmail.com>
2022-06-12 16:44:12 +0200

    detail commandes

--- pages/commandesclient/[commandeId].js
+++ pages/commandesclient/[commandeId].js
@@ -1,6 +1,9 @@
 import { useState } from "react"
+import Model from "../../components/Model"
 import Navbar from "../../components/Navbar"
+import Edit from "../../Icons/Edit"
 import Trash from "../../Icons/Trash"
+import Link from "next/link"
 
 const commandeId  = props => {
 
@@ -15,19 +18,19 @@ const commandeId  = props => {
             price : 150
         },
         {
-            id : 1 ,
+            id : 2 ,
             name : "Paracétamol",
             quantity : 50,
             price : 150
         },
         {
-            id : 1 ,
+            id : 3 ,
             name : "Ventoline",
             quantity : 50,
             price : 150
         },
         {
-            id : 1 ,
+            id : 4 ,
             name : "Rhumafed",
             quantity : 50,
             price : 150
@@ -35,6 +38,8 @@ const commandeId  = props => {
     ]
 
     const [modifierBoutton , setModiferBoutton] = useState(false)
+    const [qttProductModifier , setQttProductModifier] = useState(null)
+    const [idProductModifier , setIdProductModifier] = useState(null)
 
     return(
         <div className="h-fit pb-10 w-[100vw] min-h-[100vh] text-[20px] flex font-mono justify-center bg-[#EBFFEF]">
@@ -69,24 +74,64 @@ const commandeId  = props => {
                                 produits.map((el , index) => {
                                     return(
                                         <div className="h-[120px] w-[120px] rounded-md shadow-md bg-white/50 backdrop-blur-sm flex flex-col items-center p-2 space-y-2 text-[11px] relative">
+                                            {modifierBoutton&&
+                                                <button 
+                                                    onClick={(e) => setIdProductModifier(el.id)}
+                                                    className='top-1 left-2 absolute'
+                                                >
+                                                    <Edit/> 
+                                                </button>
+                                            }
                                             <div className=" text-[14px] break-all text-[#2EAC33] font-semibold">{el.name}</div>
                                             <div className="flex flex-row">
                                                 <div>Quantitée :</div>
-                                                <div>{el.quantity}</div>
+                                                {
+                                                    idProductModifier === el.id ?
+                                                    <input
+                                                        className="h-[16px] w-[35px] shadow-md bg-slate-100 rounded-sm p-1 outline-none" 
+                                                        placeholder={el.id}
+                                                        type = "number"
+                                                        min="1"
+                                                        max="15"
+                                                        onChange = {(e) => setQttProductModifier(e.target.value)}
+                                                    />
+                                                    :
+                                                    <div>{el.quantity}</div>
+                                                }
                                             </div>
                                             <div className="flex flex-row">
                                                 <div>Prix :</div>
                                                 <div>{el.price}</div>
                                             </div>
-                                            <button className="h-[16px] text-[11px] w-fit px-2 rounded-lg shadow-lg bg-[#2EAC33]/80 hover:bg-[#2EAC33] text-white m-auto">Voir plus</button>
+                                            {
+                                                idProductModifier === el.id ? 
+                                                <button className="h-[16px] text-[11px] w-fit px-2 rounded-lg shadow-lg bg-[#2EAC33]/80 hover:bg-[#2EAC33] text-white m-auto">Valider</button>
+                                                 : 
+                                                <Link href={`/productlist/${el.id}`}>
+                                                    <button 
+                                                        className="h-[16px] text-[11px] w-fit px-2 rounded-lg shadow-lg bg-[#2EAC33]/80 hover:bg-[#2EAC33] text-white m-auto"
+                                                    >
+                                                        Voir plus
+                                                    </button>
+                                                </Link>
+                                            }
                                         </div>
                                     )
                                 })
                             }
                         </div>
                     </div>
-                    <div className="absolute bottom-4 left-6 cursor-pointer h-[30px] text-[18px] w-fit px-2 rounded-lg shadow-lg bg-[#2EAC33]/80 hover:bg-[#2EAC33] text-white">Modifier</div>
-                    <div className="absolute bottom-4 right-6 cursor-pointer h-[30px] text-[18px] w-fit px-2 rounded-lg shadow-lg bg-[#2EAC33]/80 hover:bg-[#2EAC33] text-white">Supprimer</div>
+                    <div 
+                        className="absolute bottom-4 left-6 cursor-pointer h-[30px] text-[18px] w-fit px-2 rounded-lg shadow-lg bg-[#2EAC33]/80 hover:bg-[#2EAC33] text-white"
+                        onClick={(e) => setModiferBoutton(true)}
+                    >
+                        Modifier
+                    </div>
+                    <div 
+                        className="absolute bottom-4 right-6 cursor-pointer h-[30px] text-[18px] w-fit px-2 rounded-lg shadow-lg bg-[#2EAC33]/80 hover:bg-[#2EAC33] text-white"
+                    >
+                        Supprimer
+                    </div>
                 </div>
             </div>
         </div>
